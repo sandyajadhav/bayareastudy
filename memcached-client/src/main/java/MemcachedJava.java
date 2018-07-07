ef4f0a5d8a92013c9d3a90b70baecafd6a064f44
sandeepjadhav <sandeep.jadhav.ap@nielsen.com>
2018-07-07 15:45:58 -0700

    updated server

--- memcached-client/src/main/java/MemcachedJava.java
+++ memcached-client/src/main/java/MemcachedJava.java
@@ -10,12 +10,14 @@ public class MemcachedJava {
     public static void main(String[] args) throws IOException {
 
         // Connecting to Memcached server on localhost
-        MemcachedClient mcc = new MemcachedClient(new
-                InetSocketAddress("my-first-memcached1.ca2pee.0001.usw2.cache.amazonaws.com", 11211));
+        //MemcachedClient mcc = new MemcachedClient(new InetSocketAddress("my-first-memcached1.ca2pee.0001.usw2.cache.amazonaws.com", 11211));
+
+        MemcachedClient mcc = new MemcachedClient(new InetSocketAddress("127.0.0.1", 11111));
+
         System.out.println("Connection to server sucessfully");
 
         //not set data into memcached server
-        System.out.println("set status:"+mcc.set("tutorialspoint", 900, "memcached").isDone());
+        System.out.println("set status:"+mcc.set("tutorialspoint", 900, "memcached").getStatus().isSuccess());
 
         //Get value from cache
         System.out.println("Get from Cache:"+mcc.get("tutorialspoint"));
